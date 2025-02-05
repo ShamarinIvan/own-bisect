0d1afcf29579af831e86c673250198aa294ed6ea
ShamarinIvan <schaiv09@gmail.com>
2025-02-06 01:23:49 +0700

    Add tests for subtract function

--- test_calculator.py
+++ test_calculator.py
@@ -1,11 +1,16 @@
 import unittest
-from calculator import add
+from calculator import add, subtract
 
 class TestCalculator(unittest.TestCase):
     def test_add(self):
         self.assertEqual(add(2, 3), 5)
         self.assertEqual(add(-1, 1), 0)
         self.assertEqual(add(0, 0), 0)
+
+    def test_subtract(self):
+        self.assertEqual(subtract(5, 3), 2)
+        self.assertEqual(subtract(0, 0), 0)
+        self.assertEqual(subtract(-1, 1), -2)
 
 if __name__ == "__main__":
     unittest.main()
